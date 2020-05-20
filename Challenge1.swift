dfcdc1cec33eb278a5bb0c4218fd3f98eeda1b8f
Venkata Sudhakar <venkatasudhakar33@gmail.com>
2020-05-20 12:01:07 +0530

    challege1

---
+++ Challenge1.swift
@@ -0,0 +1,21 @@
+//
+//  Challenge1.swift
+//  SwiftUI-Practices
+//
+//  Created by venkata sudhakar on 20/05/20.
+//  Copyright © 2020 venkata sudhakar. All rights reserved.
+//
+
+import SwiftUI
+
+struct Challenge1: View {
+    var body: some View {
+      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Challenge1_Previews: PreviewProvider {
+    static var previews: some View {
+        Challenge1()
+    }
+}
